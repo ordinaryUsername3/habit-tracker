e973d5891c20c8e326d1ac07e30c532b6a0e28d9
ordinary <ordinaryUsername@outlook.com>
2025-06-29 01:35:53 +0200

    fix(backend): correct pre-save hook function and password length enforcement

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,20 +1,21 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const asyncHandler = require('express-async-handler');
 
 const userSchema = new mongoose.Schema({
     name: {type: String, required: true, trim: true, lowercase: true},
     age: {type: Number, min: 0, required: true},
     email: {type: String, unique: true, required: true, lowercase: true, trim: true},
-    password: {type: String, min:10, required: true}
+    password: {type: String, minlength:10, required: true}
 },
 {timestamps: true});
 
-userSchema.pre('save', asyncHandler(async (next) => {
+userSchema.pre('save',async function(next) {
     if (!this.isModified('password')) return next();
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
     next();
-}));
+});
+
+
 
 module.exports = mongoose.model('User', userSchema);
