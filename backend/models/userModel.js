84b0b28a12aab0be7218f5c8cea1c048d58b305d
ordinary <ordinaryUsername@outlook.com>
2025-06-29 01:37:01 +0200

    feat(backend): add password comparison method to user schema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -16,6 +16,9 @@ userSchema.pre('save',async function(next) {
     next();
 });
 
+userSchema.methods.comaprePassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
 
 
 module.exports = mongoose.model('User', userSchema);
