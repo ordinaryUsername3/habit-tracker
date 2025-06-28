7a12090019b60ce993f8f9a170ec968a89f54e79
ordinary <ordinaryUsername@outlook.com>
2025-06-28 23:56:47 +0200

    ensure DB is connected before starting server

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const notFound = require('./middleware/notFound');
+const errorHandler = require('./middleware/errorHandler');
 
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 const app = express();
 
+// middleware
+//core
+
+
+//routes
+
+
+//not found
+app.use(notFound);
+
+//error-handler
+app.use(errorHandler);
+
 app.get('/', (req, res) => res.send('Hello, world!'));
 
-app.listen(PORT, () => {
-    console.log(`server running on http://localhost:${PORT}`);
-    connectDB();
-});
+const startServer = async () {
+    try {
+        await connectDB();
+        app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
+    } catch (err) {
+        console.error('Startup failed: ', err);
+        process.exit(1);
+    }
+}
+startServer();
