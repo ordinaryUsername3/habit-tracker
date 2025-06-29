58da4859855be916d140640488a4c248f7a12f1b
ordinary <ordinaryUsername@outlook.com>
2025-06-29 02:23:30 +0200

    fix(backend): fixed dollar signs use for route params and remove async-handler import

--- backend/routes/habitRoutes.js
+++ backend/routes/habitRoutes.js
@@ -1,11 +1,11 @@
-const asyncHandler = require('express-async-handler');
 const express = require('express');
 const router = express.Router();
+const {addHabit, getHabit, getHabits, updateHabit, deleteHabit} = require('./../controllers/habitController')
 
 router.post('/', addHabit);
-router.get('/$id', getHabit);
+router.get('/:id', getHabit);
 router.get('/', getHabits);
-router.put('/$id', updateHabit);
-router.delete('/$id', deleteHabit);
+router.put('/:id', updateHabit);
+router.delete('/:id', deleteHabit);
 
 module.exports = router;
