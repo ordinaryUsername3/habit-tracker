bf0b992d6744281cab3460da70f50a63f84c5c1e
ordinary <ordinaryUsername@outlook.com>
2025-06-29 23:40:23 +0200

    feat(backend):util that sends the token via cookies

---
+++ backend/utils/setCookies.js
@@ -0,0 +1,13 @@
+const setCookies = (res, token) => {
+    const cookieOptions = {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'Strict',
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+        ),
+    }
+    res.cookie('token', token, cookieOptions);
+}
+
+module.exports = setCookies;
