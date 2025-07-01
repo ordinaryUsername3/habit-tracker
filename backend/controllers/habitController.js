77d91891a31c2d54ff0f7430f3647075c805ccf6
ordinary <ordinaryUsername@outlook.com>
2025-07-01 19:49:21 +0200

    refactor(controllers): use req.user.id from auth middleware

--- backend/controllers/habitController.js
+++ backend/controllers/habitController.js
@@ -5,7 +5,7 @@ const AppError = require('./../utils/AppError');
 
 const addHabit = asyncHandler(
     async (req, res) => {
-        const userId = req.user._id;
+        const userId = req.user.id;
         const {title, description, frequency, status, streak} = req.body;
         if (!title || !frequency || !status || !streak) {
             throw new AppError('All fields are mandatory except description', 400);
@@ -25,7 +25,7 @@ const addHabit = asyncHandler(
 const getHabit = asyncHandler(
     async (req, res) => {
         const habitId = req.params.id;
-        const userId = req.user._id;
+        const userId = req.user.id;
         if (!habitId) throw new AppError('MIssing ID for requested habit', 400);
 
         const habit = await Habit.findOne({_id: habitId, user: userId});
@@ -38,7 +38,7 @@ const getHabit = asyncHandler(
 );
 const getHabits = asyncHandler(
     async (req, res) => {
-        const userId = req.user._id;
+        const userId = req.user.id;
         const habits = await Habit.find({user: userId});
         const message = habits.length ? 'Habits fetched': 'User has no habits';
         const data = habits ? habits:[];
@@ -48,7 +48,7 @@ const getHabits = asyncHandler(
 
 const updateHabit = asyncHandler(
     async (req, res) => {
-        const userId = req.user._id;
+        const userId = req.user.id;
         const habitId = req.params.id;
         const updates = req.body;
         if (!habitId) {
@@ -64,7 +64,7 @@ const updateHabit = asyncHandler(
 );
 const deleteHabit = asyncHandler(
     async (req, res) => {
-        const userId = req.user._id;
+        const userId = req.user.id;
         const habitId = req.params.id;
         if (!habitId) {
             throw new AppError('Deletion failed: Invalid habit id', 400);
@@ -78,3 +78,5 @@ const deleteHabit = asyncHandler(
     }
 );
 
+
+module.exports = {addHabit, getHabit, getHabits, updateHabit, deleteHabit};
