f415afb1627d5c6621a17b62f562d1be06bed67f
ordinary <ordinaryUsername@outlook.com>
2025-07-04 22:12:33 +0200

    feat(backend): add standard fields for the frequency using enum

--- backend/models/habitModel.js
+++ backend/models/habitModel.js
@@ -4,7 +4,9 @@ const habitSchema = new mongoose.Schema({
     title: {type: String, required: true, trim: true},
     description: {type: String, default: '', trim: true},
     status: {type: String, required: true, enum: ['completed', 'pending', 'paused']},
-    frequency: {type: Number, required: true},
+    frequency: {type: Number, required: true, 
+        emum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Daily', 'Weekly', 'Monthly']
+    },
     streak: {type: Number, required: true, default: 0},
     user: {type: mongoose.Schema.ObjectId, ref: 'User', required: true}
 },
