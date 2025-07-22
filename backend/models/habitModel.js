4204c47b8b8bf4b6aeea579c49f920fbf640c889
ordinary <ordinaryUsername@outlook.com>
2025-07-22 11:32:43 +0200

    refactor(backend): change frequency schema to a number 1 - 7 from using days

--- backend/models/habitModel.js
+++ backend/models/habitModel.js
@@ -3,11 +3,9 @@ const mongoose = require('mongoose');
 const habitSchema = new mongoose.Schema({
     title: {type: String, required: true, trim: true},
     description: {type: String, default: '', trim: true},
-    status: {type: String, required: true, enum: ['completed', 'pending', 'paused']},
-    frequency: {type: String, required: true, 
-        emum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Daily', 'Weekly', 'Monthly']
-    },
-    streak: {type: Number, required: true, default: 0},
+    status: {type: String, enum: ['completed', 'pending', 'paused'], default: 'pending'},
+    frequency: {type: Number, required: true, min:1, max:7, default: 1},
+    streak: {type: Number, default: 0},
     user: {type: mongoose.Schema.ObjectId, ref: 'User', required: true}
 },
 {timestamps: true});
