2715a99be6699d273e4472907b5a3a88a991771a
ordinary <ordinaryUsername@outlook.com>
2025-08-04 16:18:20 +0200

    feat(frontend): Use auth interceptor in habit API calls

--- frontend/src/features/habit/habitThunk.js
+++ frontend/src/features/habit/habitThunk.js
@@ -1,44 +1,44 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
 const URL = import.meta.env.VITE_API_URL + '/api/habits';
+import { authInterceptor } from "../user/setupInterceptor";
 
 
 export const fetchHabits=createAsyncThunk('habit/fetchHabits', () => {
-    return axios.get(URL, {withCredentials: true}).then(
+    return authInterceptor.get(URL, {withCredentials: true}).then(
         (res) => res.data.data //list of habits
     )
 });
 export const addHabit=createAsyncThunk('habit/addHabit', (reqData) => {
-    return axios.post(URL, reqData, {withCredentials: true}).then(
+    return authInterceptor.post(URL, reqData, {withCredentials: true}).then(
         (res) => res.data.data
     )
 });
 export const updateHabit=createAsyncThunk('habit/updateHabit', ({id, reqData}) => {
-    return axios.put(URL + `/${id}`, reqData, {withCredentials: true}).then(
+    return authInterceptor.put(URL + `/${id}`, reqData, {withCredentials: true}).then(
         (res) => res.data.data // how to use an parameter
     )
 });
 export const deleteHabit=createAsyncThunk('habit/deleteHabit', (id) => {
-    return axios.delete(URL + `/${id}`, {withCredentials: true}).then(
+    return authInterceptor.delete(URL + `/${id}`, {withCredentials: true}).then(
         (res) => res.data.data
     )
 });
 
 export const completeHabit=createAsyncThunk('habit/completeHabit', (habitId) => {
-    return axios.put(URL + `/complete/${habitId}`,{},{withCredentials: true}).then(
+    return authInterceptor.put(URL + `/complete/${habitId}`,{},{withCredentials: true}).then(
         res => res.data.data
     )
 });
 
 export const restoreHabit=createAsyncThunk('habit/restoreHabit', (habitId) => {
-    return axios.put(URL + `/restore/${habitId}`, {}, {withCredentials: true}).then(
+    return authInterceptor.put(URL + `/restore/${habitId}`, {}, {withCredentials: true}).then(
         res => res.data.data
     )
 });
 
 
 export const pauseHabit=createAsyncThunk('habit/pauseHabit', (habitId) => {
-    return axios.put(URL + `/pause/${habitId}`, {}, {withCredentials: true}).then(
+    return authInterceptor.put(URL + `/pause/${habitId}`, {}, {withCredentials: true}).then(
         res => res.data.data
     )
 })
